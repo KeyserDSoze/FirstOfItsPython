33dee6540311ae7c7888bf8218d89c637618f44b
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2022-10-27 18:32:08 +0200

    code normalization and delegation for agnostic input/output

--- FirstOfItsPython/FirstOfItsPython.py
+++ FirstOfItsPython/FirstOfItsPython.py
@@ -1,23 +1,6 @@
-from asyncio.windows_events import NULL
 from IdentityCard import IdentityCard
-from typing import List
 from Storage.SqlIntegration import *
 from Settings.AppSettings import *
-
-ala = None
-
-try:
-    print(ala.a())
-except:
-    print("ala doesn't exist")
-
-if ala is None:
-    print("ala doesn't exist")
-else:
-   ala.a()
-
-
-#print(ala.a())
 
 settings = AppSettings("appsettings.json")
 var = SqlIntegration(settings.database_connection)
@@ -37,51 +20,12 @@ print(var.execute_query('''
             '''))
 print(var.read_query("select * from [dbo].[Table_IdentityCards]"))
 
-q = list(["apple", "cherry"])
-q.append("banana")
-print(q)
-for k in q:
-    print(k)
-
-x =  set(("apple", "banana", "cherry"))
-x.add("dasdsada")
-print(x)
-
-for t in x:
-    print(t)
-
-thistuple = tuple(("apple", "banana", "apple", "cherry")) # note the double round-brackets
-print(thistuple)
-print(thistuple.count("apple"))
-print(len(thistuple))
-for x in thistuple:
-    print(x)
-
-
-
-identity_cards: List[IdentityCard] = []
-print(identity_cards)
 print("insert the word 'e' to exit")
 insertSomething = input()
 while insertSomething != "e":
-    identity_card = IdentityCard()
-    print("Insert forename")
-    identity_card.forename = input()
-    print("Insert lastname")
-    identity_card.lastname = input()
-    print("Insert city")
-    identity_card.city = input()
-    identity_cards.append(identity_card)
-    print(len(identity_cards))
-    print(identity_card)
-    print("insert the word 'e' to exit")
-    insertSomething = input()
-
-for identity_card in identity_cards:
-    print(identity_card.get_values())
-print(identity_cards)
-
-for identity_card in identity_cards:
+    identity_card = IdentityCard(print, input)
+    identity_card.set_values()
+    identity_card.set_values2()
     var.execute_query(f'''
         Insert into Table_IdentityCards (ForeName, LastName, City) values (
             '{identity_card.forename}',
@@ -89,4 +33,8 @@ for identity_card in identity_cards:
             '{identity_card.city}'
             )
             ''')
+    print("insert the word 'e' to exit")
+    insertSomething = input()
+
+    
 print(var.read_query("select * from [dbo].[Table_IdentityCards]"))
--- FirstOfItsPython/IdentityCard.py
+++ FirstOfItsPython/IdentityCard.py
@@ -4,5 +4,20 @@ class IdentityCard:
     lastname = ""
     city = ""
 
+    def __init__(self, output_method, input_method):
+        self.output_method = output_method
+        self.input_method = input_method
+
     def get_values(self):
         return self.forename + " " + self.lastname + " from " + self.city
+
+    def set_values(self):
+        self.output_method("Insert forename")
+        self.forename = self.input_method()
+        self.output_method("Insert lastname")
+        self.lastname = self.input_method()
+        
+        
+    def set_values2(self):
+        self.output_method("Insert city")
+        self.city = self.input_method()
